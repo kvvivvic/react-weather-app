6f6932eda3cdf7b9e70f34b00fb134397aca6c23
sido <rnjs9337@gmail.com>
2023-03-30 02:55:42 +0900

    axios로 날씨 데이터 받아와서 화면에 출력 시키기

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import axios from "axios";
 
 function App() {
   const [location, setLocation] = useState("");
+  const [data, setData] = useState({});
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
-  const handleChange = (e) => setLocation(e.target.value);
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = await axios({
-      method: "get",
-      url: url,
-    }).catch(console.log("error"));
-    console.log(data);
-    setLocation("");
+    const data = await axios.get(url).then((res) => res.data);
+    setData(data);
   };
   return (
-    <>
-      <div>hi</div>
+    <div className="w-96 mx-auto ">
       <form onSubmit={handleSubmit}>
         <input
           className="border border-red-500"
@@ -24,9 +22,17 @@ function App() {
           value={location}
           onChange={handleChange}
         />
+        <button>click</button>
       </form>
-      <div></div>
-    </>
+      {Object.keys(data).length !== 0 && (
+        <div>
+          <div>{data.name}</div>
+          <div>{Math.round((data.main.temp - 273.15) * 10) / 10}</div>
+          <div>{data.weather[0].description}</div>
+          <div>{data.weather[0].main}</div>
+        </div>
+      )}
+    </div>
   );
 }
 
